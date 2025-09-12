752a0ad590f0e5f41fac7fb80c50731076936772
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-09-12 23:38:16 +0530

    demoshopapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Item from './components/item'
-
+import ItemDate from './components/ItemDate';
 
 function App() {
   return (
     <div>
     <Item></Item>
+    <ItemDate></ItemDate>
     <div className="App">Hello jee</div>
     </div>
   );

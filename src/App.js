388fcf7b855a02e21a6301e9b1c7ef91d6547446
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-09-12 22:52:57 +0530

    demoshopapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './components/item'
 
 function App() {
   return (
-    <div className="App">
-      Hello jee
+    <div>
+    <Item></Item>
+    <div className="App">Hello jee</div>
     </div>
   );
 }
---
+++ src/components/item.js
@@ -0,0 +1,6 @@
+function Item(){
+    return(<p className="nirma">Nirma</p>)
+
+}
+
+export default Item;
